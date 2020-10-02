120f8315b5e88eae48d68a54f6183742ac485e62
dman2210 <32994314+dman2210@users.noreply.github.com>
2020-10-02 12:04:38 -0600

    added passing test

---
+++ src/toolkit/PerceptronTest.java
@@ -0,0 +1,20 @@
+package toolkit;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PerceptronTest {
+
+    @org.junit.jupiter.api.Test
+    void getWeightDelta() {
+        Random rand = new Random();
+        Perceptron ptron = new Perceptron(rand);
+        double learningRate = 0.2;
+        double activation = 1.0;
+        double target = 0.0;
+        double input = 0.87;
+        double result = learningRate*(target-activation)*input;
+        assertEquals(ptron.getWeightDelta(activation, input, target),result);
+    }
+}
